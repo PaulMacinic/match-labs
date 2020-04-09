0a633d958e217c5bd20658420e08d73b54350e16
Paul Macinic <paulmacinic@gmail.com>
2020-04-09 16:28:24 +0300

    exercise3

---
+++ exerciseDescription.js
@@ -0,0 +1,13 @@
+// Create a components folder inside the /src folder
+
+// 1. Create a functional component named PageTitle
+
+// 2. Create a folder called components inside the /src folder and add the component there
+
+// 3. Return a <h1> with some text, inside the PageTitle component
+
+// 4. Import the component inside App.js and render it to the page using:
+/* import PageTitle from "./components/PageTitle" */
+
+// 5. In your App.js render the component by simply using:
+/* return <PageTitle/> */
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+/* import PageTitle from "./components/PageTitle" */
+
+const App = () => {
+  // Render your PageTitle component here
+  return <p>Hello Match Labs</p>;
+};
+
+export default App;
