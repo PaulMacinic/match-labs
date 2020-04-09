d2928c35d1449ade88ef070f3cca161a8ee6c452
Paul Macinic <paulmacinic@gmail.com>
2020-04-09 16:40:08 +0300

    exercise2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-alert("If you see this, you successfully installed your first React App! See you on Thursday!");
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+// Here you will render your component using ReactDOM.render()
+// The first argument is your component
+// The second argument is the element on the page where it should render, in our case document.querySelector("#root")
