9319e320a996193600aba2042d8a3d93ec5a0119
Paul Macinic <paulmacinic@gmail.com>
2020-05-06 21:15:09 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ const App = () => {
 
   return (
     // 1. Pass setUser to Context
-    <AppContext.Provider value={{ user, setUser }}>
+    <AppContext.Provider value={{ user }}>
       <BrowserRouter>
         <Toggle />
         <Switch>
