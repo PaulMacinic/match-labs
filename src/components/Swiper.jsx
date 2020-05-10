308760955c3f4074b6664cd19f9a49393fb3a659
Paul Macinic <paulmacinic@gmail.com>
2020-05-10 21:21:26 +0300

    animations done

--- src/components/Swiper.jsx
+++ src/components/Swiper.jsx
@@ -4,55 +4,80 @@ import styles from "./Swiper.module.css";
 import Card from "./Card";
 import { AppContext } from "../Context";
 import Loader from "./Loader";
+import { CSSTransition } from "react-transition-group";
+import { useState } from "react";
 
 const Swiper = ({ items, callback }) => {
   const { user } = useContext(AppContext);
+  const [values, setValues] = useState({ swiped: true, direction: "" });
 
   const next = items[items.length - 2];
   const current = items[items.length - 1];
 
-  const onButtonClick = async () => {
-    callback();
+  const onButtonClick = async (direction) => {
+    setValues({ swiped: false, direction });
+  };
+
+  const reset = () => {
+    setValues({ swiped: true, direction: "" });
   };
 
   const _renderButtons = () => {
     return (
       <div className={styles.buttons}>
-        <div className={styles.button} onClick={() => onButtonClick()}>
+        <div className={styles.button} onClick={() => onButtonClick("left")}>
           <img src={require("../static/svg/close.svg")} alt={"close"} />
         </div>
-        <div className={styles.button} onClick={() => onButtonClick()}>
+        <div className={styles.button} onClick={() => onButtonClick("right")}>
           <img src={require("../static/svg/heart.svg")} alt={"heart"} />
         </div>
       </div>
     );
   };
 
-  if (!items) return <Loader></Loader>;
+  if (!items.length) return <Loader></Loader>;
 
   return (
     <section className={styles.swiper}>
       <div className={styles.content}>
-        <Link to={`/profile/${next.id}`} className={`${styles.cardContainer}`}>
-          <Card
-            outline={user.role === "candidate"}
-            name={next.name}
-            imgUrl={next.profile_image}
-            technologies={next.technologies}
-          ></Card>
-        </Link>
+        {items.length > 1 && (
+          <Link
+            to={`/profile/${next.id}`}
+            className={`${styles.cardContainer}`}
+          >
+            <Card
+              outline={user.role === "candidate"}
+              name={next.name}
+              imgUrl={next.profile_image}
+              technologies={next.technologies}
+            ></Card>
+          </Link>
+        )}
 
-        <Link
-          to={`/profile/${current.id}`}
-          className={`${styles.cardContainer} `}
+        <CSSTransition
+          key={current.id}
+          in={values.swiped}
+          timeout={300}
+          classNames={{ exit: styles.exit, exitDone: styles.exit }}
+          onExited={() => {
+            reset();
+            callback();
+          }}
         >
-          <Card
-            outline={user.role === "candidate"}
-            name={current.name}
-            imgUrl={current.profile_image}
-            technologies={current.technologies}
-          ></Card>
-        </Link>
+          <Link
+            to={`/profile/${current.id}`}
+            className={`${styles.cardContainer} ${
+              styles[values.direction] || ""
+            } `}
+          >
+            <Card
+              outline={user.role === "candidate"}
+              name={current.name}
+              imgUrl={current.profile_image}
+              technologies={current.technologies}
+            ></Card>
+          </Link>
+        </CSSTransition>
       </div>
 
       {_renderButtons()}
