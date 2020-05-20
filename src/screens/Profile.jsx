a525208291dbece02788cc5b126b8a3a6dd2c21c
Paul Macinic <paulmacinic@gmail.com>
2020-05-20 12:24:05 +0300

    Update Profile.jsx

--- src/screens/Profile.jsx
+++ src/screens/Profile.jsx
@@ -5,18 +5,15 @@ import Card from "../components/Card";
 import { fetchProfile, fetchLikes, like, dislike } from "../utils/request";
 import Loader from "../components/Loader";
 import GoBack from "../components/GoBack";
-import Match from "../components/Match";
 
 const Profile = (props) => {
   const [profile, setProfile] = useState(null);
   const [likes, setLikes] = useState(null);
-  const [match, setMatch] = useState(null);
 
   useEffect(() => {
     const onMount = async () => {
       const id = props.match.params.id;
       const profile = await fetchProfile(id);
-      // 1. fetchLikes and setLikes
       const likes = await fetchLikes();
 
       setLikes(likes);
@@ -26,22 +23,24 @@ const Profile = (props) => {
   }, [props.match.params.id]);
 
   const removeLike = () => {
-    // 5. Find like index and splice it from state
-  };
-
-  const onMatch = () => {
-    // 6. Find like  and set it as match
-  };
-
-  const onContinueSwiping = () => {
-    // resetMatch
-    setMatch(null);
+    const newLikes = [...likes];
+    const index = newLikes.findIndex((like) => like.id === profile.id);
+    newLikes.splice(index, 1);
+    setLikes(newLikes);
   };
 
   const onButtonClick = async (direction) => {
-    // 4. removeLike from state
-    //  likeOrDislike
-    // setMatch
+    direction === "right" ? await like(profile.id) : await dislike(profile.id);
+    // 1. if match call onMatch()
+    removeLike();
+  };
+
+  const onMatch = () => {
+    // 2. Find like and set it as match
+  };
+
+  const onContinueSwiping = () => {
+    // 4.  resetMatch
   };
 
   const _renderButtons = () => {
@@ -58,43 +57,39 @@ const Profile = (props) => {
   };
 
   const isLikeable = () => {
-    // 3. likes.some
+    return likes.some((likes) => likes.id === profile.id);
   };
 
   if (!profile) return <Loader />;
 
   return (
-    <>
-      <div className={styles.profile}>
-        <GoBack />
-        <div className={styles.hero}>
-          <Card imgUrl={profile.profile_image}></Card>
-          {/* 2. If likeable render buttons */}
-        </div>
-        <div className={styles.rightSide}>
-          <h3 className={styles.name}>{profile.name}</h3>
-
-          <section className={styles.skills}>
-            <p className={styles.tagsTitle}>Technologies</p>
-            {profile.technologies && <Tags tags={profile.technologies}></Tags>}
-          </section>
-
-          <section className={styles.objectives}>
-            <h4 className={styles.heading}>Objectives</h4>
-            <p>{profile.objectives}</p>
-          </section>
-
-          <section className={styles.description}>
-            <h4 className={styles.heading}>About</h4>
-
-            <p>{profile.description}</p>
-          </section>
-        </div>
-        {match && (
-          <Match match={match} onContinueSwiping={onContinueSwiping}></Match>
-        )}
+    <div className={styles.profile}>
+      <GoBack />
+      <div className={styles.hero}>
+        <Card imgUrl={profile.profile_image}></Card>
+        {isLikeable() && _renderButtons()}
       </div>
-    </>
+      <div className={styles.rightSide}>
+        <h3 className={styles.name}>{profile.name}</h3>
+
+        <section className={styles.skills}>
+          <p className={styles.tagsTitle}>Technologies</p>
+          {profile.technologies && <Tags tags={profile.technologies}></Tags>}
+        </section>
+
+        <section className={styles.objectives}>
+          <h4 className={styles.heading}>Objectives</h4>
+          <p>{profile.objectives}</p>
+        </section>
+
+        <section className={styles.description}>
+          <h4 className={styles.heading}>About</h4>
+
+          <p>{profile.description}</p>
+        </section>
+      </div>
+      {/* 3. set match in state and append the Match component */}
+    </div>
   );
 };
 
